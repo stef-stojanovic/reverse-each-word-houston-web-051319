0009ef4f5b75c5256df7c83873fbdb910fbc3dfe
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-19 12:52:29 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  sentence.reverse.split.reverse.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
 binding.pry
